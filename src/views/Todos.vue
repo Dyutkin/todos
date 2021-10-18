d6f9bf5421b2a24156b3c986413dc5bdd1576962
i.dyutkin <ilia.diutkin@stm-labs.ru>
2021-10-18 14:43:22 +0300

    todo

---
+++ src/views/Todos.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+    <h2>Todo application</h2>
+    <router-link to="/">Home</router-link>
+    <hr>
+    <AddTodo
+        @add-todo="addTodo"
+    />
+    <select v-model="filter">
+      <option value="all">All</option>
+      <option value="completed">Completed</option>
+      <option value="not-completed">Not Completed</option>
+    </select>
+    <hr>
+    <Loader v-if="loading" />
+    <TodoList
+        v-else-if="filteredTodos.length"
+        v-bind:todos="filteredTodos"
+        @remove-todo="removeTodo"
+    />
+    <p v-else>No todos!</p>
+  </div>
+</template>
+
+<script>
+import TodoList from '@/components/TodoList'
+import AddTodo from '@/components/AddTodo'
+import Loader from '@/components/Loader'
+export default {
+  name: 'app',
+  data() {
+    return {
+      todos: [],
+      loading: true,
+      filter: 'all',
+      filteredTodos: [],
+    }
+  },
+  mounted() {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=3')
+      .then(response => response.json())
+      .then(json => {
+        setTimeout(() => {
+          this.todos = json;
+          this.filteredTodos = json;
+          this.loading = false;
+        }, 1000)
+
+      })
+  },
+  // watch: {
+  //    filter(value) {
+  //     console.log(value)
+  //    if (this.filter === 'all') {
+  //       this.filteredTodos = this.todos;
+  //     }
+
+  //     if (this.filter === 'completed') {
+  //       this.filteredTodos = this.todos.filter(t => t.completed);
+  //     }
+
+  //     if (this.filter === 'not-completed') {
+  //       this.filteredTodos = this.todos.filter(t => !t.completed);
+  //     }
+  //   }
+  // },
+  computed: {
+    filteredTodos() {
+      if (this.filter === 'all') {
+        return this.todos
+      }
+
+      if (this.filter === 'completed') {
+        return this.todos.filter(t => t.completed)
+      }
+
+      if (this.filter === 'not-completed') {
+        return this.todos.filter(t => !t.completed)
+      }
+    }
+  },
+  methods: {
+    removeTodo(id) {
+      this.todos = this.todos.filter(t => t.id !== id)
+    },
+    addTodo(todo) {
+      this.todos.push(todo)
+    }
+  },
+  components: {
+    TodoList, AddTodo, Loader
+  }
+}
+</script>
